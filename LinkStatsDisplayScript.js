16490015833d060c9be49e7a3a702543779147d9
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2021-11-19 11:11:10 -0800

    Update LinkStatsDisplayScript.js

--- LinkStatsDisplayScript.js
+++ LinkStatsDisplayScript.js
@@ -3,7 +3,7 @@ function dispLinkStats() {
     for(ele in elements) {
         try {
             element = elements[ele];
-            if(!!element.textContent.trim() && !element.stat) {
+            if(!!element.textContent.trim() && !element.stat && element.href) {
                 elementImg = element.appendChild(document.createElement("img"));
                 elementImg.src = "https://s2.googleusercontent.com/s2/favicons?domain=" + element.href
                 elementImg.width = "15";
